0c7dbbd6e01a61f1fa528073d62d497cf8e3316f
sajjad-ali96 <sajjad.casanova@gmail.com>
2020-11-14 18:51:22 +0500

    first commit

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 
 const PORT = process.env.PORT || 2000;
 const api = require('./routes/api')
 const app = express();
 app.use(cors())
 
+const STATIC = path.resolve(__dirname, 'public', 'dist');
+const INDEX = path.resolve(STATIC, 'index.html');
+
+app.use(express.static(STATIC));
+
 app.use(bodyParser.json())
 app.use('/api', api)
 
 
-app.get('/', (req, res) => {
-    res.send('server is on...!!')
-})
+app.get('*', (req, res) => {
+    res.sendFile(INDEX);
+});
+
 
 app.listen(PORT, () => {
     console.log('Server is running on localhost:' + PORT)
